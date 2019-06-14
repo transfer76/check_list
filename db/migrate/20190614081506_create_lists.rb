9287075825a41b86679315173bef2b8bb9c56464
transfer76 <shina76@ukr.net>
2019-06-14 11:22:13 +0300

    Create models and add rspec

---
+++ db/migrate/20190614081506_create_lists.rb
@@ -0,0 +1,13 @@
+class CreateLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :lists do |t|
+      t.string :title
+      t.string :description
+      t.string :project_id
+      t.integer :table
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
