87d8e8c8d3e7997e7c2fe366fcf50a9ff29575f8
transfer76 <shina76@ukr.net>
2019-07-02 19:22:08 +0300

    Add dependence destroy question to check_list

--- app/models/check_list.rb
+++ app/models/check_list.rb
@@ -2,7 +2,7 @@ class CheckList < ApplicationRecord
   belongs_to :form
   belongs_to :user
 
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   validates :project_uid, presence: true
 
