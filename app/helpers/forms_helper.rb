065d5111417494524b289d5e8266e301eb6a38fb
transfer76 <shina76@ukr.net>
2019-07-03 02:02:35 +0300

    Modified form and check_list helpers

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,2 +1,17 @@
 module FormsHelper
+  def form_status_text(form)
+    if form.published?
+      'Published'
+    else
+      "Draft (#{form.updated_at.strftime('%d.%m.%Y')})"
+    end
+  end
+
+  def form_published_text(form)
+    if form.published?
+      form.published_at.strftime('%d.%m.%Y')
+    else
+      "Not published"
+    end
+  end
 end
