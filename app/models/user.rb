10147e92457dcca1c09e4a097df6119dc0def085
transfer76 <shina76@ukr.net>
2019-07-11 10:30:26 +0300

    Commented out function admin for production

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,13 @@ class User < ApplicationRecord
     has_role?(:admin)
   end
 
-  def admin
-    admin?
-  end
+  # test admin for production
 
-  def admin=(value)
-    add_role(:admin) if value && value == '1'
-  end
+  # def admin
+  #   admin?
+  # end
+
+  # def admin=(value)
+  #   add_role(:admin) if value && value == '1'
+  # end
 end
